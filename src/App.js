4c862f23deec82be3901253ecceb2bf3794befd2
Louisss-Lam <louislam617@gmail.com>
2022-12-01 14:37:35 +0000

    useState setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="container">
       <Header />
